c6ec8d562f997ef411585e0f38ad416f0fd836b8
neochrist <kikouchia58@gmail.com>
2022-11-09 13:13:15 +0400

    adjusted

--- db/migrations/20221107122926_create_tables.sql
+++ db/migrations/20221107122926_create_tables.sql
@@ -15,7 +15,7 @@ CREATE TABLE persons(
 	person_id INT AUTO_INCREMENT NOT NULL COMMENT "Unique person id",
 	person_name VARCHAR(50) NOT NULL COMMENT "Person's first name",
 	person_surname VARCHAR(50) NOT NULL COMMENT "Person's last name",
-	city_name VARCHAR(50) COMMENT "The city name",
+	city_id INT "The City Id of the cities table",
 	PRIMARY KEY(person_id)
 
 	ENGINE=InnoDB
